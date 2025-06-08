2ff74b76a0a9b9a99b6459eca094fec9a178314a
SeetaramNaik <seetaramnaik44@gmail.com>
2025-06-08 21:26:28 +0530

    Initial commit

---
+++ src/main/java/com/seetu/hello_world/controller/Hello.java
@@ -0,0 +1,13 @@
+package com.seetu.hello_world.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController()
+public class Hello {
+
+    @GetMapping("/hi")
+    public String hi() {
+        return "Hello World";
+    }
+
+}
